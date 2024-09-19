197c395a8020a60608a3ee8b5bbedf6cb62edf52
carolinafelix <carolinafelix.dev@gmail.com>
2024-09-19 19:13:37 -0300

    feature/screenStay

--- src/models/Program.java
+++ src/models/Program.java
@@ -1,10 +1,16 @@
 package models;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.ParseException;
+
+import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
 public class Program {
@@ -21,43 +27,59 @@ class TestandoJFormattedTextField extends JFrame {
         Container janela = getContentPane();
         setLayout(null);
 
-        // Define os rótulos
-        JLabel labelCep = new JLabel("CEP: ");
-        JLabel labelTel = new JLabel("Telefone: ");
-        JLabel labelCpf = new JLabel("CPF: ");
-        JLabel labelData = new JLabel("Data: ");
-        labelCep.setBounds(50, 40, 100, 20);
-        labelTel.setBounds(50, 80, 100, 20);
-        labelCpf.setBounds(50, 120, 100, 20);
-        labelData.setBounds(50, 160, 100, 20);
+        
+        setTitle("Dados de Estadia");
 
-        // Define as máscaras
-        MaskFormatter mascaraCep = createMaskFormatter("#####-###");
-        MaskFormatter mascaraTel = createMaskFormatter("(##)####-####");
-        MaskFormatter mascaraCpf = createMaskFormatter("#########-##");
-        MaskFormatter mascaraData = createMaskFormatter("##/##/####");
+       
+        JLabel labelRoomNumber = new JLabel("Room Number:");
+        JLabel labelStartDate = new JLabel("Start Date:");
+        JLabel labelFinalDate = new JLabel("Final Date:");
+        labelRoomNumber.setBounds(50, 40, 100, 20);
+        labelStartDate.setBounds(50, 80, 100, 20);
+        labelFinalDate.setBounds(50, 120, 100, 20);
+       
+    
+        JButton btnSubmit = new JButton("Enviar");
+        btnSubmit.setBounds(150, 160, 80, 30); // Ajustando a posição do botão
+        
+   
+        MaskFormatter mascaraRoomNumber = createMaskFormatter("####");
+        MaskFormatter mascaraStartDate = createMaskFormatter("##/##/####");
+        MaskFormatter mascaraFinalDate = createMaskFormatter("##/##/####");
 
-        // Seta as máscaras nos JFormattedTextField
-        JFormattedTextField jFormattedTextCep = new JFormattedTextField(mascaraCep);
-        JFormattedTextField jFormattedTextTel = new JFormattedTextField(mascaraTel);
-        JFormattedTextField jFormattedTextCpf = new JFormattedTextField(mascaraCpf);
-        JFormattedTextField jFormattedTextData = new JFormattedTextField(mascaraData);
-        jFormattedTextCep.setBounds(150, 40, 100, 20);
-        jFormattedTextTel.setBounds(150, 80, 100, 20);
-        jFormattedTextCpf.setBounds(150, 120, 100, 20);
-        jFormattedTextData.setBounds(150, 160, 100, 20);
+        
+        JFormattedTextField jFormattedTextRoomNumber = new JFormattedTextField(mascaraRoomNumber);
+        JFormattedTextField jFormattedTextStartDate = new JFormattedTextField(mascaraStartDate);
+        JFormattedTextField jFormattedTextFinalDate = new JFormattedTextField(mascaraFinalDate);
+        jFormattedTextRoomNumber.setBounds(150, 40, 30, 20);
+        jFormattedTextStartDate.setBounds(150, 80, 70, 20);
+        jFormattedTextFinalDate.setBounds(150, 120, 70, 20);
+        
 
-        // Adiciona os rótulos e campos de texto
-        janela.add(labelCep);
-        janela.add(labelTel);
-        janela.add(labelCpf);
-        janela.add(labelData);
-        janela.add(jFormattedTextCep);
-        janela.add(jFormattedTextTel);
-        janela.add(jFormattedTextCpf);
-        janela.add(jFormattedTextData);
+        
+        janela.add(labelRoomNumber);
+        janela.add(labelStartDate);
+        janela.add(labelFinalDate);
+        janela.add(jFormattedTextRoomNumber);
+        janela.add(jFormattedTextStartDate);
+        janela.add(jFormattedTextFinalDate);
+        janela.add(btnSubmit);
+    
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String roomNumber = jFormattedTextRoomNumber.getText();
+                String startDate = jFormattedTextStartDate.getText();
+                String finalDate = jFormattedTextFinalDate.getText();
 
-        setSize(400, 250);
+                // Criando o pop-up com os dados
+                String message = String.format("Room Number: %s\nStart Date: %s\nFinal Date: %s", 
+                                                roomNumber, startDate, finalDate);
+                JOptionPane.showMessageDialog(null, message, "Dados Enviados", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        
+        setSize(400, 250); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
@@ -70,7 +92,7 @@ class TestandoJFormattedTextField extends JFrame {
         } catch (ParseException e) {
             System.err.println("Erro na formatação: " + e.getMessage());
             System.exit(-1);
-            return null; // Unreachable, but required for compilation
+            return null; 
         }
     }
 }
